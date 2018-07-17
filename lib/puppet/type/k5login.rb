7f23ffd42ab0ae1ca2fc5e7da4a7e4911c0b8b36
Melissa Stone <melissa@puppet.com>
2018-07-17 16:10:03 -0700

    Automatic rubocop corrections
    These were generated by running `bundle exec rubocop -a`

--- lib/puppet/type/k5login.rb
+++ lib/puppet/type/k5login.rb
@@ -11,52 +11,52 @@ Puppet::Type.newtype(:k5login) do
   ensurable
 
   # Principals that should exist in the file
-  newproperty(:principals, :array_matching => :all) do
-    desc "The principals present in the `.k5login` file. This should be specified as an array."
+  newproperty(:principals, array_matching: :all) do
+    desc 'The principals present in the `.k5login` file. This should be specified as an array.'
   end
 
   # The path/name of the k5login file
   newparam(:path) do
     isnamevar
-    desc "The path to the `.k5login` file to manage.  Must be fully qualified."
+    desc 'The path to the `.k5login` file to manage.  Must be fully qualified.'
 
     validate do |value|
       unless absolute_path?(value)
-        raise Puppet::Error, _("File paths must be fully qualified.")
+        raise Puppet::Error, _('File paths must be fully qualified.')
       end
     end
   end
 
   # To manage the mode of the file
   newproperty(:mode) do
-    desc "The desired permissions mode of the `.k5login` file. Defaults to `644`."
-    defaultto { "644" }
+    desc 'The desired permissions mode of the `.k5login` file. Defaults to `644`.'
+    defaultto { '644' }
   end
 
   # To manage the selinux user of the file
-  newproperty(:seluser, :parent => Puppet::SELFileContext) do
+  newproperty(:seluser, parent: Puppet::SELFileContext) do
     desc "What the SELinux user component of the context of the file should be.
       Any valid SELinux user component is accepted.  For example `user_u`.
       If not specified it defaults to the value returned by matchpathcon for
       the file, if any exists.  Only valid on systems with SELinux support
       enabled."
 
-    defaultto { "user_u" }
+    defaultto { 'user_u' }
   end
 
   # To manage the selinux role of the file
-  newproperty(:selrole, :parent => Puppet::SELFileContext) do
+  newproperty(:selrole, parent: Puppet::SELFileContext) do
     desc "What the SELinux role component of the context of the file should be.
       Any valid SELinux role component is accepted.  For example `role_r`.
       If not specified it defaults to the value returned by matchpathcon for
       the file, if any exists.  Only valid on systems with SELinux support
       enabled."
 
-    defaultto { "object_r" }
+    defaultto { 'object_r' }
   end
 
   # To manage the selinux type of the file
-  newproperty(:seltype, :parent => Puppet::SELFileContext) do
+  newproperty(:seltype, parent: Puppet::SELFileContext) do
     desc "What the SELinux type component of the context of the file should be.
       Any valid SELinux type component is accepted.  For example `tmp_t`.
       If not specified it defaults to the value returned by matchpathcon for
@@ -64,11 +64,11 @@ Puppet::Type.newtype(:k5login) do
       enabled."
 
     # to my knowledge, `krb5_home_t` is the only valid type for .k5login
-    defaultto { "krb5_home_t" }
+    defaultto { 'krb5_home_t' }
   end
 
   # To manage the selinux range of the file
-  newproperty(:selrange, :parent => Puppet::SELFileContext) do
+  newproperty(:selrange, parent: Puppet::SELFileContext) do
     desc "What the SELinux range component of the context of the file should be.
       Any valid SELinux range component is accepted.  For example `s0` or
       `SystemHigh`.  If not specified it defaults to the value returned by
@@ -76,7 +76,7 @@ Puppet::Type.newtype(:k5login) do
       SELinux support enabled and that have support for MCS (Multi-Category
       Security)."
 
-    defaultto { "s0" }
+    defaultto { 's0' }
   end
 
   # Stat our file.
@@ -94,7 +94,7 @@ Puppet::Type.newtype(:k5login) do
     rescue Errno::ENOTDIR
       nil
     rescue Errno::EACCES
-      warning _("Could not stat; permission denied")
+      warning _('Could not stat; permission denied')
       nil
     end
   end
@@ -104,7 +104,7 @@ Puppet::Type.newtype(:k5login) do
     super
   end
 
-  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
   provide(:k5login) do
     desc "The k5login provider is the only provider for the k5login
@@ -121,7 +121,7 @@ Puppet::Type.newtype(:k5login) do
     def create
       write(@resource.should(:principals))
       should_mode = @resource.should(:mode)
-      unless self.mode == should_mode
+      unless mode == should_mode
         self.mode = should_mode
       end
     end
@@ -134,7 +134,7 @@ Puppet::Type.newtype(:k5login) do
     # Return the principals
     def principals
       if Puppet::FileSystem.exist?(@resource[:name])
-        File.readlines(@resource[:name]).collect { |line| line.chomp }
+        File.readlines(@resource[:name]).map { |line| line.chomp }
       else
         :absent
       end
@@ -147,7 +147,7 @@ Puppet::Type.newtype(:k5login) do
 
     # Return the mode as an octal string, not as an integer
     def mode
-      "%o" % (Puppet::FileSystem.stat(@resource[:name]).mode & 007777)
+      '%o' % (Puppet::FileSystem.stat(@resource[:name]).mode & 0o07777)
     end
 
     # Set the file mode, converting from a string to an integer.
@@ -156,8 +156,9 @@ Puppet::Type.newtype(:k5login) do
     end
 
     private
+
     def write(value)
-      Puppet::Util.replace_file(@resource[:name], 0644) do |f|
+      Puppet::Util.replace_file(@resource[:name], 0o644) do |f|
         f.puts value
       end
     end
--- spec/unit/type/k5login_spec.rb
+++ spec/unit/type/k5login_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 require 'fileutils'
 require 'puppet/type'
 
-describe Puppet::Type.type(:k5login), :unless => Puppet.features.microsoft_windows? do
-
-  context "the type class" do
+describe Puppet::Type.type(:k5login), unless: Puppet.features.microsoft_windows? do
+  context 'the type class' do
     subject { described_class }
+
     it { is_expected.to be_validattr :ensure }
     it { is_expected.to be_validattr :path }
     it { is_expected.to be_validattr :principals }
@@ -23,13 +23,13 @@ describe Puppet::Type.type(:k5login), :unless => Puppet.features.microsoft_windo
 
   def resource(attrs = {})
     attrs = {
-      :ensure     => 'present',
-      :path       => path,
-      :principals => 'fred@EXAMPLE.COM',
-      :seluser    => 'user_u',
-      :selrole    => 'role_r',
-      :seltype    => 'type_t',
-      :selrange   => 's0',
+      ensure: 'present',
+      path: path,
+      principals: 'fred@EXAMPLE.COM',
+      seluser: 'user_u',
+      selrole: 'role_r',
+      seltype: 'type_t',
+      selrange: 's0',
     }.merge(attrs)
 
     if content = attrs.delete(:content)
@@ -44,64 +44,64 @@ describe Puppet::Type.type(:k5login), :unless => Puppet.features.microsoft_windo
     FileUtils.touch(path)
   end
 
-  context "the provider" do
-    context "when the file is missing" do
-      it "should initially be absent" do
+  context 'the provider' do
+    context 'when the file is missing' do
+      it 'initiallies be absent' do
         File.delete(path)
         expect(resource.retrieve[:ensure]).to eq(:absent)
       end
 
-      it "should create the file when synced" do
-        resource(:ensure => 'present').parameter(:ensure).sync
-        expect(Puppet::FileSystem.exist?(path)).to be_truthy
+      it 'creates the file when synced' do
+        resource(ensure: 'present').parameter(:ensure).sync
+        expect(Puppet::FileSystem).to be_exist(path)
       end
     end
 
-    context "when the file is present" do
-      context "retrieved initial state" do
+    context 'when the file is present' do
+      context 'retrieved initial state' do
         subject { resource.retrieve }
 
-        it "should retrieve its properties correctly with zero principals" do
+        it 'retrieves its properties correctly with zero principals' do
           expect(subject[:ensure]).to eq(:present)
           expect(subject[:principals]).to eq([])
           # We don't really care what the mode is, just that it got it
           expect(subject[:mode]).not_to be_nil
         end
 
-        context "with one principal" do
-          subject { resource(:content => "daniel@EXAMPLE.COM\n").retrieve }
+        context 'with one principal' do
+          subject { resource(content: "daniel@EXAMPLE.COM\n").retrieve }
 
-          it "should retrieve its principals correctly" do
-            expect(subject[:principals]).to eq(["daniel@EXAMPLE.COM"])
+          it 'retrieves its principals correctly' do
+            expect(subject[:principals]).to eq(['daniel@EXAMPLE.COM'])
           end
         end
 
         [:seluser, :selrole, :seltype, :selrange].each do |param|
           property = described_class.attrclass(param)
           context param.to_s do
-            let(:sel_param) { property.new :resource => resource }
+            let(:sel_param) { property.new resource: resource }
 
-            context "with selinux" do
-              it "should return correct values based on SELinux state" do
+            context 'with selinux' do
+              it 'returns correct values based on SELinux state' do
                 allow(sel_param).to receive(:debug)
                 expectedresult = case param
-                  when :seluser; "user_u"
-                  when :selrole; "object_r"
-                  when :seltype; "krb5_home_t"
-                  when :selrange; "s0"
+                                 when :seluser then 'user_u'
+                                 when :selrole then 'object_r'
+                                 when :seltype then 'krb5_home_t'
+                                 when :selrange then 's0'
                 end
                 expect(sel_param.default).to eq(expectedresult)
               end
             end
 
             context 'without selinux' do
-              it 'should not try to determine the initial state' do
+              it 'does not try to determine the initial state' do
                 allow(Puppet::Type::K5login::ProviderK5login.any_instance).to receive(:selinux_support?).and_return false
 
                 expect(subject[:selrole]).to be_nil
               end
 
-              it "should do nothing for safe_insync? if no SELinux support" do
+              it 'does nothing for safe_insync? if no SELinux support' do
                 sel_param.should = 'newcontext'
                 expect(sel_param).to receive(:selinux_support?).and_return false
                 expect(sel_param.safe_insync?('oldcontext')).to eq(true)
@@ -110,58 +110,58 @@ describe Puppet::Type.type(:k5login), :unless => Puppet.features.microsoft_windo
           end
         end
 
-        context "with two principals" do
+        context 'with two principals' do
           subject do
-            content = ["daniel@EXAMPLE.COM", "george@EXAMPLE.COM"].join("\n")
-            resource(:content => content).retrieve
+            content = ['daniel@EXAMPLE.COM', 'george@EXAMPLE.COM'].join("\n")
+            resource(content: content).retrieve
           end
 
-          it "should retrieve its principals correctly" do
-            expect(subject[:principals]).to eq(["daniel@EXAMPLE.COM", "george@EXAMPLE.COM"])
+          it 'retrieves its principals correctly' do
+            expect(subject[:principals]).to eq(['daniel@EXAMPLE.COM', 'george@EXAMPLE.COM'])
           end
         end
       end
 
-      it "should remove the file ensure is absent" do
-        resource(:ensure => 'absent').property(:ensure).sync
-        expect(Puppet::FileSystem.exist?(path)).to be_falsey
+      it 'removes the file ensure is absent' do
+        resource(ensure: 'absent').property(:ensure).sync
+        expect(Puppet::FileSystem).not_to be_exist(path)
       end
 
-      it "should write one principal to the file" do
-        expect(File.read(path)).to eq("")
-        resource(:principals => ["daniel@EXAMPLE.COM"]).property(:principals).sync
+      it 'writes one principal to the file' do
+        expect(File.read(path)).to eq('')
+        resource(principals: ['daniel@EXAMPLE.COM']).property(:principals).sync
         expect(File.read(path)).to eq("daniel@EXAMPLE.COM\n")
       end
 
-      it "should write multiple principals to the file" do
-        content = ["daniel@EXAMPLE.COM", "george@EXAMPLE.COM"]
+      it 'writes multiple principals to the file' do
+        content = ['daniel@EXAMPLE.COM', 'george@EXAMPLE.COM']
 
-        expect(File.read(path)).to eq("")
-        resource(:principals => content).property(:principals).sync
+        expect(File.read(path)).to eq('')
+        resource(principals: content).property(:principals).sync
         expect(File.read(path)).to eq(content.join("\n") + "\n")
       end
 
-      describe "when setting the mode" do
+      describe 'when setting the mode' do
         # The defined input type is "mode, as an octal string"
-        ["400", "600", "700", "644", "664"].each do |mode|
+        ['400', '600', '700', '644', '664'].each do |mode|
           it "should update the mode to #{mode}" do
-            resource(:mode => mode).property(:mode).sync
+            resource(mode: mode).property(:mode).sync
 
-            expect((Puppet::FileSystem.stat(path).mode & 07777).to_s(8)).to eq(mode)
+            expect((Puppet::FileSystem.stat(path).mode & 0o7777).to_s(8)).to eq(mode)
           end
         end
       end
 
-      context "#stat" do
-        let(:file) { described_class.new(:path => path) }
+      context '#stat' do
+        let(:file) { described_class.new(path: path) }
 
-        it "should return nil if the file does not exist" do
+        it 'returns nil if the file does not exist' do
           file[:path] = make_absolute('/foo/bar/baz/non-existent')
 
           expect(file.stat).to be_nil
         end
 
-        it "should return nil if the file cannot be stat'ed" do
+        it "returns nil if the file cannot be stat'ed" do
           dir = tmpfile('link_test_dir')
           child = File.join(dir, 'some_file')
 
@@ -178,11 +178,11 @@ describe Puppet::Type.type(:k5login), :unless => Puppet.features.microsoft_windo
             expect(file.stat).to be_nil
           ensure
             # chmod it back so we can clean it up
-            File.chmod(0777, dir)
+            File.chmod(0o777, dir)
           end
         end
 
-        it "should return nil if parts of path are not directories" do
+        it 'returns nil if parts of path are not directories' do
           regular_file = tmpfile('ENOTDIR_test')
           FileUtils.touch(regular_file)
           impossible_child = File.join(regular_file, 'some_file')
@@ -191,11 +191,11 @@ describe Puppet::Type.type(:k5login), :unless => Puppet.features.microsoft_windo
           expect(file.stat).to be_nil
         end
 
-        it "should return the stat instance" do
+        it 'returns the stat instance' do
           expect(file.stat).to be_a(File::Stat)
         end
 
-        it "should cache the stat instance" do
+        it 'caches the stat instance' do
           expect(file.stat.object_id).to eql(file.stat.object_id)
         end
       end
