41534cdc0ed294e5625ef7f0d849ec54d22455a3
Melissa Stone <melissa@puppet.com>
2018-07-17 17:45:01 -0700

    Fix rubocop C: Style/GuardClause

--- lib/puppet/type/k5login.rb
+++ lib/puppet/type/k5login.rb
@@ -121,9 +121,7 @@ Puppet::Type.newtype(:k5login) do
     def create
       write(@resource.should(:principals))
       should_mode = @resource.should(:mode)
-      unless mode == should_mode
-        self.mode = should_mode
-      end
+      self.mode = should_mode unless mode == should_mode
     end
 
     # remove the file
